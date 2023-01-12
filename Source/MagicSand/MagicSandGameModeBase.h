c2241fa459d51bc6aeb825183c36dbbe2e39fea7
HerraHinrik <herrahinrik@gmail.com>
2023-01-12 17:30:22 +0100

    git fun

---
+++ Source/MagicSand/MagicSandGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MagicSandGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICSAND_API AMagicSandGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
